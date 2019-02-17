fb4a313461de1e94a3df9ee72c7793634b0e4754
StevenGhub <stevenhuang5132@gamil.com>
2019-02-16 18:33:51 -0800

    dummy attact

--- dummy.js
+++ dummy.js
@@ -5,13 +5,14 @@ function Dummy(game) {
 // Animation object: spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse
    //right
    this.walkRightAnim = new Animation(AM.getAsset("./img/macewindu_right.png"), 893 ,632, -37, 60, 0.2, 11, true, false);
-   this.standAnim = new Animation(AM.getAsset("./img/macewindu_right.png") , 893, 75, -30, 65, 1, 1, true, false);
-   this.attackAnim = new Animation(AM.getAsset("./img/macewindu_right.png"), 893, 1745, -80, 78, 0.2, 3, true, false);
+   this.standAnim = new Animation(AM.getAsset("./img/macewindu_right.png") , 895, 70, -35, 60, 1, 1, true, false);
+   this.attackRightAnim = new Animation(AM.getAsset("./img/macewindu_right.png"), 880, 1790, -90, 70, 0.2, 3, true, false);
 
    //left
    this.walkLeftAnim = new Animation(AM.getAsset("./img/macewindu_left.png"), 7, 632, 37, 60, 0.2, 11, true, false);
-   this.startAnim = new Animation(AM.getAsset("./img/macewindu_left.png"), 0, 1655, 67, 85, 0.3, 4, true, false);
-   this.thinkAnim = new Animation(AM.getAsset("./img/macewindu_left.png"), 0, 0, 50, 66, 0.7, 4, true, false);
+   this.standLeftAnim = new Animation(AM.getAsset("./img/macewindu_left.png") , 0, 70, 40, 55, 1, 1, true, false);
+   this.attackLeftAnim = new Animation(AM.getAsset("./img/macewindu_left.png"), 20, 1790, 90, 70, 0.2, 8, true, false);
+
 
    this.begin = true;
    this.speed = 100;
@@ -36,15 +37,20 @@ Dummy.prototype = new Entity();
 Dummy.prototype.constructor = Dummy;
 
 Dummy.prototype.update = function (){
-   this.distance = this.player.x + 50 - this.x; 
+   this.distance = this.player.x + 50 - this.x;
+   this.attack = false;
+   this.standing = false;
 
    if (this.distance > 50) {
       this.x += this.game.clockTick * this.speed;
    } else if (this.distance < -50) {
       this.x -= this.game.clockTick * this.speed;
+   } else if ( Math.abs(this.player.y - this.y) < 50){
+      this.attack = true;
+   } else {
+      this.standing = true;
    }
 
-   console.log(this.game.entities[0]);
 
   
 
@@ -62,11 +68,24 @@ Dummy.prototype.draw = function() {
 
 Dummy.prototype.drawRight = function() {
 
-   this.walkRightAnim.drawFrame(this.game.clockTick, this.ctx, this.x + 50, this.y, scale);
+   if (this.attack) {
+      this.attackRightAnim.drawFrame(this.game.clockTick, this.ctx, this.x + 65, this.y - 10, scale);
+   } else if (this.standing){
+      this.standAnim.drawFrame(this.game.clockTick, this.ctx, this.x + 20, this.y + 5, scale);
+   } else {
+      this.walkRightAnim.drawFrame(this.game.clockTick, this.ctx, this.x + 50, this.y + 5, scale);
+   }
    Entity.prototype.draw.call(this);
 }
 
 Dummy.prototype.drawLeft = function() {
-   this.walkLeftAnim.drawFrame(this.game.clockTick, this.ctx, this.x , this.y, scale);
+
+   if (this.attack) {
+      this.attackLeftAnim.drawFrame(this.game.clockTick, this.ctx, this.x -50, this.y - 10, scale);
+   } else if (this.standing){
+      this.standLeftAnim.drawFrame(this.game.clockTick, this.ctx, this.x, this.y + 5, scale);
+   } else {
+      this.walkLeftAnim.drawFrame(this.game.clockTick, this.ctx, this.x , this.y + 5, scale);
+   }
    Entity.prototype.draw.call(this);
 }
